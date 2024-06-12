6019ec6be6d98884499afabfb237410923eac140
Lucas Quinteiro Campos <136658353+Lucasqc04@users.noreply.github.com>
2024-06-12 19:38:54 -0300

    Add files via upload

---
+++ Lucas_site/src/main/java/Lucas/com/Lucas_site/LucasSiteApplication.java
@@ -0,0 +1,13 @@
+package Lucas.com.Lucas_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucasSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucasSiteApplication.class, args);
+	}
+
+}
---
+++ Lucas_site/src/test/java/Lucas/com/Lucas_site/LucasSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package Lucas.com.Lucas_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucasSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
